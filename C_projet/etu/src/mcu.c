05929f787a1a8cbf8ac7a2082a2895bf85b803e4
zarbiaa <amine.zarbi@grenoble-inp.org>
2018-05-24 11:47:58 +0200

    commit

--- C_projet/etu/src/mcu.c
+++ C_projet/etu/src/mcu.c
@@ -28,34 +28,55 @@ struct image *decoupage_8x8(FILE* fichier) {
     struct image *mcu = malloc(sizeof(struct image));
 
     struct entete_PPM *entete =  lecture_entete_PPM(fichier);
-    uint16_t nbr_samples = ceil(entete->largeur / 8 ) * ceil(entete->hauteur / 8 );
-    //printf("%u\n", nbr_samples);
+    uint16_t nbr_samples = ceil((float) entete->largeur / 8 ) * ceil((float) entete->hauteur / 8 );
+    // printf("%u  %u   %u\n",entete->largeur/8, entete->hauteur/8, nbr_samples);
     mcu->mcu_table = calloc(nbr_samples,sizeof(uint16_t* ));
     mcu->size = nbr_samples;
     for (uint16_t i = 0; i < nbr_samples; i++){
         mcu->mcu_table[i] = calloc(64, sizeof(uint16_t));
     }
-    // uint32_t nv_largeur = ceil(entete->largeur / 8)*8;
-    // uint32_t nv_hauteur = ceil(entete->hauteur / 8)*8;
+    uint32_t nv_largeur = ceil(entete->largeur / 8)*8;
+    uint32_t nv_hauteur = ceil(entete->hauteur / 8)*8;
     // for (uint32_t i = 0; i < nbr_samples*64; i++){
     //     uint32_t i_sample_number = ceil(entete->largeur/8)/8 + ceil(entete->hauteur/8)/8;
     // }
+    uint32_t sample_number;
+    uint16_t index_mcu;
     uint32_t i = 0;
     int16_t c = fgetc(fichier);
-    while (c != EOF ){
-          uint32_t sample_number = (i%entete->largeur)/8 + (entete->hauteur/8)*((i/entete->hauteur)/8);
-          uint16_t index_in_mcu = 8*((i/entete->hauteur)%8) + (i%entete->largeur)%8;
+
+    while ( i < nv_largeur*nv_hauteur ){
+          printf("%u\n", i);
+          if (i% nv_largeur < entete->largeur && i/nv_hauteur < entete->hauteur){
+            /* code */
+          sample_number = (i%entete->largeur)/8 + (entete->hauteur/8)*((i/entete->hauteur)/8);
+          index_mcu = 8*((i/entete->hauteur)%8) + (i%entete->largeur)%8;
           //printf("%i\t%i\t %i\n", sample_number, index_in_mcu, i);
-          mcu->mcu_table[sample_number][index_in_mcu] = c;
+          mcu->mcu_table[sample_number][index_mcu] = c;
           i++;
           c = fgetc(fichier);
+
+
+        }  else if (i%nv_largeur >= entete->largeur && i%nv_hauteur < entete->hauteur) {
+          mcu->mcu_table[sample_number][index_mcu+1] = c;
+          index_mcu++;
+          i++;
+        } else {
+          uint32_t i_bis = i - nv_largeur;
+          uint32_t sample_number_bis = (i_bis%entete->largeur)/8 + (entete->hauteur/8)*((i_bis/entete->hauteur)/8);
+          uint16_t index_mcu_bis = 8*((i_bis/entete->hauteur)%8) + (i_bis%entete->largeur)%8;
+          mcu->mcu_table[sample_number_bis][index_mcu_bis + 8] = mcu->mcu_table[sample_number_bis][index_mcu_bis];
+          i++;
+        }
+
+
         }
 
     return mcu;
 
 }
 
-void afficher_mcu(int16_t *tableau) {
+void afficher_mcu(uint16_t *tableau) {
   for (int16_t i = 0; i < 64; i++) {
     printf("%04hx\t", tableau[i]);
     if ( (i+1) % 8 == 0){
@@ -65,9 +86,9 @@ void afficher_mcu(int16_t *tableau) {
 }
 
 void afficher(struct image *mcus){
-  for (uint16_t i=0; i < mcus->size ; i++){
+  for (uint16_t i=61; i < 65 ; i++){
    printf(" ITER %u\n", i );
-    afficher_mcu(mcus->table_quantif[i]);
+    afficher_mcu(mcus->mcu_table[i]);
     printf("\n\n");
   }
 }
